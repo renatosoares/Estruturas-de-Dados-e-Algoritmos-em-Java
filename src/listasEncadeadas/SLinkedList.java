8c50ffd92e341b0ff5cff94b5189085be4b85df0
Renato Soares <renatosoarespro@hotmail.com>
2015-10-30 13:44:41 -0300

    insersão de um elemento na cabeça de uma lista simplesmente encadeada

--- src/listasEncadeadas/SLinkedList.java
+++ src/listasEncadeadas/SLinkedList.java
@@ -11,4 +11,11 @@ public class SLinkedList {
 		size = 0;
 	}
 	// ...os métodos de pesquisa e atualização vão aqui ...
+	
+	// insersão de um elemento na cabeça de uma lista simplesmente encadeada
+	public void addFirst(Node v) {
+		v.setNext(head);
+		head = v;
+		size = size + 1;
+	}
 }
